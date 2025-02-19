8e545773c88f2ef2d16a9c09ac223300a74045c6
arantesandre <arantes.andreluiz@gmail.com>
2025-02-18 22:11:43 -0300

    first commit

---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+// import EmojiPicker from "emoji-picker-react";
+import "./Body.css";
+
+const Body = () => {
+
+    const [image, setImage] = useState(['😍', '👍', '🤣', "😒", "❤️", "💖", "😎", "😉", "🤦‍♀️", "🤦‍♂️", "👌", "🎶", "🤩", "🫥", "😶‍🌫️", "🤐", "😜", "😓", "🤑", "😰", "🤠", "😈", "👽", "💀"])
+    // const [index, setIndex] = useState(length)
+    const [tela, setTela] = useState('')
+
+    const sortImage = () => {
+
+        let i = Math.floor(Math.random() * image.length)
+        // console.log("meu indice: " + i)
+        // console.log("meu numero do vetor: " + image[i])
+        setTela(image[i])
+    }
+
+    // utilizar useEffect para carregar um emoji
+
+    useEffect(() => {
+        sortImage()
+    })
+
+    // useEffect(() => {
+    //     const tempo = setInterval(() => {
+    //         sortImage();
+    //     }, 1000);
+
+
+    //     return () => clearInterval(tempo);
+    // }, []);
+
+    return (
+        <div className="container-body">
+            <h1 className="title">Story Cube</h1>
+
+            {/* <button className="container-body-button" onClick={sortImage}>Imagem aleatória</button> */}
+
+            {/* {'numero vetor' + image} */}
+
+            {<div className="container-emoji" onClick={sortImage}>
+                {tela}
+            </div> || <div className="container-emoji" onClick={sortImage}>
+                    {tela}
+                </div>}
+
+
+            {/* {image && <EmojiPicker />} */}
+        </div>
+
+    )
+}
+
+export default Body;
---
+++ src/components/Imagem/Imagem.jsx
@@ -0,0 +1,11 @@
+import "./Imagem.css";
+
+const Imagem = () =>{
+
+    return(
+        <img src="" alt="imagem" />
+    )
+
+}
+
+export default Imagem;
